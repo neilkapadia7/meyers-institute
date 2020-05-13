9288b0fc69d687b00cf1491425b5f001a5f90de3
neilkapadia7 <neilkapadia7@gmail.com>
2020-05-13 11:46:39 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.json({ msg: 'Welcome to the API' });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server Started on Port Number: ${PORT}`));
