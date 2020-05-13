9a693be2019b0a14f7b6a72b815608b4687a1683
neilkapadia7 <neilkapadia7@gmail.com>
2020-05-13 23:47:54 +0530

    Redux Boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => {
 	return (
-		<div className='App'>
-			<h2>Test</h2>
-		</div>
+		<Provider store={store}>
+			<div className='App'>
+				<h2>Test</h2>
+			</div>
+		</Provider>
 	);
 };
 
