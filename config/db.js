f75689354ab22997b58708a2e907dba0f40b1b61
neilkapadia7 <neilkapadia7@gmail.com>
2024-01-03 23:02:18 +0530

    New Mongo Setup

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const config = require('config');
 
 const connectDB = async () => {
 	try {
-		mongoose.connect(config.get('mongoURI'), {
+		mongoose.connect(process.env.MONGO_URI, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 		});
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const fileUpload = require('express-fileupload');
+require("dotenv").config();
 
 const app = express();
 
@@ -23,6 +24,6 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/guest', require('./routes/guest'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server Started on Port Number: ${PORT}`));
