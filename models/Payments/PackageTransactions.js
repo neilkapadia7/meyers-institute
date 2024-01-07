d6f1b3cd11e271f0dbcf7d1e6cde3844083e02e2
neilkapadia7 <neilkapadia7@gmail.com>
2024-01-07 23:28:41 +0530

    Package Transaction Schema Added

---
+++ models/Payments/PackageTransactions.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const PackageTransactions = mongoose.Schema({
+        gateway: {
+          type: String,
+          enum: ['razorpay', 'easebuzz', 'payu', 'na'],
+          default: 'na',
+        },
+        isFreeTransaction: { type: Boolean, default: false },
+        razorpaySignature: { type: String },
+        razorpayOrderId: { type: String },
+        razorpayTransactionId: { type: String },
+        razorpayStatus: { type: String },
+        razorpayTransactionAmount: { type: Number },
+        packagePrice: { type: Number },
+        packageId: { type: mongoose.Schema.Types.ObjectId, ref: 'Packages' },
+        voucherId: { type: String },
+        voucher: { type: mongoose.Schema.Types.ObjectId, ref: 'Vouchers' },
+        userId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Users"
+        },
+}, 
+{
+    timestamps: {
+      createdAt: true,
+      updatedAt: true,
+    },
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+}
+);
+
+module.exports = mongoose.model('PackageTransactions', PackageTransactions);
